dad336251aceecaff14376cf1ac14e497b52e35a
iManPost <iveenvon@gmail.com>
2026-07-05 15:19:49 +0700

    Add files via upload

---
+++ folder3/data/netflix.js
@@ -0,0 +1,201 @@
+export const netflixMovies = [
+
+
+     {
+    name: 'เมืองสีดำ 2026 ',
+    group: '🎬netflix2026',
+    logo: 'https://9.9nung.com/wp-content/uploads/2026/06/kqGS7zqOjMK6LsV6XjKUz5yreYZ-185x278.jpg',
+    url: 'https://media.vdohls.com/quH0Pj_t-vK5r/playlist.m3u8'
+  },
+     
+     {
+    name: 'ภูเขามิอาจกั้น ',
+    group: '🎬netflix2026',
+    logo: 'https://www.nunghd4k.com/wp-content/uploads/2023/11/The-Father-Who-Moves-Mountains-2021.jpg',
+    url: 'https://api.p2p-cdnmovie.xyz/1080p/68vhtm/68vhtm/index.m3u8'
+  },
+    
+     {
+    name: 'ARQ ',
+    group: '🎬netflix2026',
+    logo: 'https://www.nunghd4k.com/wp-content/uploads/2023/11/ARQ-2016.jpg',
+    url: 'https://api.p2p-cdnmovie.xyz/1080p/6qism3/6qism3/index.m3u8'
+  },
+    {
+    name: 'คนระห่ำภารกิจเดือด',
+    group: '🎬netflix2026',
+    logo: 'https://www.nunghd4k.com/wp-content/uploads/2023/11/Extraction-.jpg.webp',
+    url: 'https://master.p2p-cdnmovie.xyz/1080p/a5cvfl/a5cvfl/index.m3u8'
+  },
+    {
+    name: 'คนเป็นฝ่านรกซอมบี้',
+    group: '🎬netflix2026',
+    logo: 'https://www.nunghd4k.com/wp-content/uploads/2023/11/Alive-Saraitda-2020.jpg',
+    url: 'https://api.p2p-cdnmovie.xyz/1080p/dvhz8i/dvhz8i/index.m3u8'
+  },
+   {
+    name: 'อธิษฐานคริสต์มาส',
+    group: '🎬netflix2026',
+    logo: 'https://9.9nung.com/wp-content/uploads/2026/06/36BnxmIvWvOsMmlQfbI0ilTfuvh-185x278.jpg',
+    url: 'https://api.p2p-cdnmovie.xyz/1080p/3w0rva/3w0rva/index.m3u8'
+  },
+   
+   {
+    name: 'Teach You a Lesson (2026) ตอนที่ 1',
+    group: '🎬netflix2026',
+    logo: 'https://9.9nung.com/wp-content/uploads/2026/06/36BnxmIvWvOsMmlQfbI0ilTfuvh-185x278.jpg',
+    url: 'https://media.vdohls.com/s3zOEFC_O-L2e/video.m3u8'
+  },
+  {
+    name: 'Teach You a Lesson (2026) ตอนที่  2',
+    group: '🎬netflix2026',
+    logo: 'https://9.9nung.com/wp-content/uploads/2026/06/36BnxmIvWvOsMmlQfbI0ilTfuvh-185x278.jpg',
+    url: 'https://media.vdohls.com/2Fa9_1DgQ-0AX/video.m3u8'
+  },
+  {
+    name: 'Teach You a Lesson (2026) ตอนที่  3',
+    group: '🎬netflix2026',
+    logo: 'https://9.9nung.com/wp-content/uploads/2026/06/36BnxmIvWvOsMmlQfbI0ilTfuvh-185x278.jpg',
+    url: 'https://media.vdohls.com/K_ob5K56-VAFc/video.m3u8'
+  },
+  {
+    name: 'Teach You a Lesson (2026) ตอนที่  4',
+    group: '🎬netflix2026',
+    logo: 'https://9.9nung.com/wp-content/uploads/2026/06/36BnxmIvWvOsMmlQfbI0ilTfuvh-185x278.jpg',
+    url: 'https://media.vdohls.com/2Tqp-Q_qBUfoH/video.m3u8'
+  },
+  {
+    name: 'Teach You a Lesson (2026) ตอนที่ 5',
+    group: '🎬netflix2026',
+    logo: 'https://9.9nung.com/wp-content/uploads/2026/06/36BnxmIvWvOsMmlQfbI0ilTfuvh-185x278.jpg',
+    url: 'https://media.vdohls.com/tOzGrIyb-p_UU/video.m3u8'
+  },
+  {
+    name: 'Teach You a Lesson (2026) ตอนที่ 6',
+    group: '🎬netflix2026',
+    logo: 'https://9.9nung.com/wp-content/uploads/2026/06/36BnxmIvWvOsMmlQfbI0ilTfuvh-185x278.jpg',
+    url: 'https://media.vdohls.com/SSU-ty3USC_vN/video.m3u8'
+  },
+  {
+    name: 'Teach You a Lesson (2026) ตอนที่  7',
+    group: '🎬netflix2026',
+    logo: 'https://9.9nung.com/wp-content/uploads/2026/06/36BnxmIvWvOsMmlQfbI0ilTfuvh-185x278.jpg',
+    url: 'https://media.vdohls.com/CejH-Am_xAgTY/video.m3u8'
+  },
+  {
+    name: 'Teach You a Lesson (2026) ตอนที่  8',
+    group: '🎬netflix2026',
+    logo: 'https://9.9nung.com/wp-content/uploads/2026/06/36BnxmIvWvOsMmlQfbI0ilTfuvh-185x278.jpg',
+    url: 'https://media.vdohls.com/ABv_5i8-iZYzz/video.m3u8'
+  },
+  {
+    name: 'Teach You a Lesson (2026) ตอนที่ 9',
+    group: '🎬netflix2026',
+    logo: 'https://9.9nung.com/wp-content/uploads/2026/06/36BnxmIvWvOsMmlQfbI0ilTfuvh-185x278.jpg',
+    url: 'https://media.vdohls.com/tgvlVVIw-y_1F/video.m3u8'
+  },
+  {
+    name: 'Teach You a Lesson (2026) ตอนที่ 10',
+    group: '🎬netflix2026',
+    logo: 'https://9.9nung.com/wp-content/uploads/2026/06/36BnxmIvWvOsMmlQfbI0ilTfuvh-185x278.jpg',
+    url: 'https://media.vdohls.com/bwmXc-PGCh_VT/video.m3u8'
+  },
+  {
+    name: 'ทนายปีศาจ ตอนที่ 1',
+    group: '🎬netflix2026',
+    logo: 'https://media.online-station.net/images/2026/06/d86a402604ec144bdb6df4961fc6caa1.jpg',
+    url: 'https://media.vdohls.com/q9oJ-rlNC_ukL/video.m3u8'
+  },
+  {
+    name: 'ทนายปีศาจ ตอนที 2',
+    group: '🎬netflix2026',
+    logo: 'https://media.online-station.net/images/2026/06/d86a402604ec144bdb6df4961fc6caa1.jpg',
+    url: 'https://media.vdohls.com/Scc_Hy2-ocrmW/video.m3u8'
+  },
+  {
+    name: 'ทนายปีศาจ ตอนที 3',
+    group: '🎬netflix2026',
+    logo: 'https://media.online-station.net/images/2026/06/d86a402604ec144bdb6df4961fc6caa1.jpg',
+    url: 'https://media.vdohls.com/L-XSBI_TfELKH/video.m3u8'
+  },
+  {
+    name: 'ทนายปีศาจ ตอนที 4',
+    group: '🎬netflix2026',
+    logo: 'https://media.online-station.net/images/2026/06/d86a402604ec144bdb6df4961fc6caa1.jpg',
+    url: 'https://media.vdohls.com/wSE-KroXm9_K8/video.m3u8'
+  },
+  {
+    name: 'ทนายปีศาจ ตอนที 5',
+    group: '🎬netflix2026',
+    logo: 'https://media.online-station.net/images/2026/06/d86a402604ec144bdb6df4961fc6caa1.jpg',
+    url: 'https://media.vdohls.com/Osj-vqP_sz6y9/video.m3u8'
+  },
+  {
+    name: 'ทนายปีศาจ ตอนที 6',
+    group: '🎬netflix2026',
+    logo: 'https://media.online-station.net/images/2026/06/d86a402604ec144bdb6df4961fc6caa1.jpg',
+    url: 'https://media.vdohls.com/5-1x_c2dYUbTp/video.m3u8'
+  },
+  {
+    name: 'ทนายปีศาจ ตอนที 7',
+    group: '🎬netflix2026',
+    logo: 'https://media.online-station.net/images/2026/06/d86a402604ec144bdb6df4961fc6caa1.jpg',
+    url: 'https://media.vdohls.com/4_G-vTzUdbTaV/video.m3u8'
+  },
+  {
+    name: 'ทนายปีศาจ ตอนที 8',
+    group: '🎬netflix2026',
+    logo: 'https://media.online-station.net/images/2026/06/d86a402604ec144bdb6df4961fc6caa1.jpg',
+    url: 'https://media.vdohls.com/gxbm5Jg_YH-ZE/video.m3u8'
+  },
+      {
+    name: 'คนมหัศจรรย์พลังรั่ว ตอนที่ 1',
+    group: '🎬netflix2026',
+    logo: 'https://www.24hd.vip/wp-content/uploads/2026/05/The-WONDERfools-2026.webp',
+    url: 'https://media.vdohls.com/FMJZr_fKXP-xf/playlist.m3u8'
+  },
+  
+     {
+    name: 'คนมหัศจรรย์พลังรั่ว ตอนที่ 2',
+    group: '🎬netflix2026',
+    logo: 'https://www.24hd.vip/wp-content/uploads/2026/05/The-WONDERfools-2026.webp',
+    url: 'https://media.vdohls.com/FMJZr_fKXP-xf/playlist.m3u8'
+  },
+    {
+    name: 'คนมหัศจรรย์พลังรั่ว ตอนที่ 3',
+    group: '🎬netflix2026',
+    logo: 'https://www.24hd.vip/wp-content/uploads/2026/05/The-WONDERfools-2026.webp',
+    url: 'https://media.vdohls.com/FMJZr_fKXP-xf/playlist.m3u8'
+  },
+      {
+    name: 'คนมหัศจรรย์พลังรั่ว ตอนที่ 4',
+    group: '🎬netflix2026',
+    logo: 'https://www.24hd.vip/wp-content/uploads/2026/05/The-WONDERfools-2026.webp',
+    url: 'https://media.vdohls.com/FMJZr_fKXP-xf/playlist.m3u8'
+  },
+        {
+    name: 'คนมหัศจรรย์พลังรั่ว ตอนที่ 5',
+    group: '🎬netflix2026',
+    logo: 'https://www.24hd.vip/wp-content/uploads/2026/05/The-WONDERfools-2026.webp',
+    url: 'https://media.vdohls.com/FMJZr_fKXP-xf/playlist.m3u8'
+  },
+      {
+    name: 'คนมหัศจรรย์พลังรั่ว ตอนที่ 6',
+    group: '🎬netflix2026',
+    logo: 'https://www.24hd.vip/wp-content/uploads/2026/05/The-WONDERfools-2026.webp',
+    url: 'https://media.vdohls.com/FMJZr_fKXP-xf/playlist.m3u8'
+  },
+        {
+    name: 'คนมหัศจรรย์พลังรั่ว ตอนที่ 7',
+    group: '🎬netflix2026',
+    logo: 'https://www.24hd.vip/wp-content/uploads/2026/05/The-WONDERfools-2026.webp',
+    url: 'https://media.vdohls.com/FMJZr_fKXP-xf/playlist.m3u8'
+  },
+    {
+    name: 'คนมหัศจรรย์พลังรั่ว ตอนที่ 8',
+    group: '🎬netflix2026',
+    logo: 'https://www.24hd.vip/wp-content/uploads/2026/05/The-WONDERfools-2026.webp',
+    url: 'https://media.vdohls.com/FMJZr_fKXP-xf/playlist.m3u8'
+  },
+
+];
