7e2fd37808d618065696ce3d35a13d13de296c5f
iManPost <iveenvon@gmail.com>
2026-07-11 18:20:29 +0700

    Create comedy.js

---
+++ folder3/data/comedy.js
@@ -0,0 +1,102 @@
+ export const anime = [
+   {
+    name: 'เป็นต่อ 2026 ตอนที่ 1',
+    group: 'comedy',
+    logo: 'https://image.onehd.net/Media/Show/263012013053241.jpg',
+    url: 'https://cdn.watchlakorn.in/egde/4dafba1117f7e1168eff22991608e424/NzAwNw==/MQ==/ZGF0YTE3LzA4LTAxLTIwMjYvUGVudG9yMjAyNS04LUphbi0yMDI2Lm1wNA==/index-v1-a1.m3u8'
+  },
+
+   {
+    name: 'เป็นต่อ 2026 ตอนที่ 2',
+    group: 'comedy',
+    logo: 'https://image.onehd.net/Media/Show/263012013053241.jpg',
+    url: 'https://cdn.watchlakorn.in/egde/1a3f32537bc01380b0cf23476774088a/NzAwNw==/Mg==/ZGF0YTE3LzE1LTAxLTIwMjYvUGVudG9yMjAyNS0xNS1KYW4tMjAyNi5tcDQ=/index-v1-a1.m3u8'
+  },
+   {
+    name: 'เป็นต่อ 2026 ตอนที่ 3',
+    group: 'comedy',
+    logo: 'https://image.onehd.net/Media/Show/263012013053241.jpg',
+    url: 'https://cdn.watchlakorn.in/egde/bde0c40ba0256c1beea31a5195a016de/NzAwNw==/Mw==/ZGF0YTE3LzIyLTAxLTIwMjYvUGVudG9yMjAyNS0yMi1KYW4tMjAyNi5tcDQ=/index-v1-a1.m3u8'
+  },
+   {
+    name: 'เป็นต่อ 2026 ตอนที่ 4',
+    group: 'comedy',
+    logo: 'https://image.onehd.net/Media/Show/263012013053241.jpg',
+    url: 'https://cdn.watchlakorn.in/egde/79cfc1d822896a91c0b86041b3a68777/NzAwNw==/NA==/ZGF0YTE3LzI5LTAxLTIwMjYvUGVudG9yMjAyNS0yOS1KYW4tMjAyNi5tcDQ=/index-v1-a1.m3u8'
+  },
+
+   {
+    name: 'เป็นต่อ 2026 ตอนที่ 5',
+    group: 'comedy',
+    logo: 'https://image.onehd.net/Media/Show/263012013053241.jpg',
+    url: 'https://cdn.watchlakorn.in/egde/6c929d9a0b24c2b76b68c8e5be99ae5f/NzAwNw==/NQ==/ZGF0YTE3LzA1LTAyLTIwMjYvUGVudG9yMjAyNS01LUZlYi0yMDI2X0hELm1wNA==/index-v1-a1.m3u8'
+  },
+
+
+   {
+    name: 'เป็นต่อ 2026 ตอนที่ 6',
+    group: 'comedy',
+    logo: 'https://image.onehd.net/Media/Show/263012013053241.jpg',
+    url: 'https://cdn.watchlakorn.in/egde/aba181610b5811c711c10c73ace4957b/NzAwNw==/Ng==/ZGF0YTE3LzEyLTAyLTIwMjYvUGVudG9yMjAyNS0xMi1GZWItMjAyNi5tcDQ=/index-v1-a1.m3u8'
+  },
+
+   {
+    name: 'เป็นต่อ 2026 ตอนที่ 7',
+    group: 'comedy',
+    logo: 'https://image.onehd.net/Media/Show/263012013053241.jpg',
+    url: 'https://cdn.watchlakorn.in/egde/5ad9e8ba46498bb31664319389a28ac9/NzAwNw==/Nw==/ZGF0YTE3LzE5LTAyLTIwMjYvUGVudG9yMjAyNS0xOS1GZWItMjAyNi5tcDQ=/index-v1-a1.m3u8'
+  },
+   {
+    name: 'เป็นต่อ 2026 ตอนที่ 8',
+    group: 'comedy',
+    logo: 'https://image.onehd.net/Media/Show/263012013053241.jpg',
+    url: 'https://cdn.watchlakorn.in/egde/905bb68510d3d52c19e01c6c0f3ba002/NzAwNw==/OA==/ZGF0YTE3LzI2LTAyLTIwMjYvUGVudG9yMjAyNS0yNi1GZWItMjAyNi5tcDQ=/index-v1-a1.m3u8'
+  },
+
+
+
+   {
+    name: 'เป็นต่อ 2026 ตอนที่ 19',
+    group: 'comedy',
+    logo: 'https://image.onehd.net/Media/Show/263012013053241.jpg',
+    url: 'https://cdn.watchlakorn.in/egde/311ac684c07318618824ecde29632df8/NzAwNw==/MTk=/ZGF0YTE3LzIxLTA1LTIwMjYvUGVudG9yMjAyNi0yMS1NYXktMjAyNi5tcDQ=/index-v1-a1.m3u8'
+  },
+
+   {
+    name: 'เป็นต่อ 2026 ตอนที่ 20',
+    group: 'comedy',
+    logo: 'https://image.onehd.net/Media/Show/263012013053241.jpg',
+    url: 'https://cdn.watchlakorn.in/egde/42adc085add8d9465ca82fb638479e28/NzAwNw==/MjA=/ZGF0YTE3LzI4LTA1LTIwMjYvUGVudG9yMjAyNi0yOC1NYXktMjAyNi5tcDQ=/index-v1-a1.m3u8'
+  },
+   {
+    name: 'เป็นต่อ 2026 ตอนที่ 21',
+    group: 'comedy',
+    logo: 'https://image.onehd.net/Media/Show/263012013053241.jpg',
+    url: 'https://cdn.watchlakorn.in/egde/b59eb12562ae72cdfa22a1557e1f71d5/NzAwNw==/MjE=/ZGF0YTE3LzA0LTA2LTIwMjYvUGVudG9yMjAyNi00LUp1bi0yMDI2Lm1wNA==/index-v1-a1.m3u8'
+  },
+   {
+    name: 'เป็นต่อ 2026 ตอนที่ 22',
+    group: 'comedy',
+    logo: 'https://image.onehd.net/Media/Show/263012013053241.jpg',
+    url: 'https://cdn.watchlakorn.in/egde/91eea4e6b7f1d1e35b5bdcb1581f87d5/NzAwNw==/MjI=/ZGF0YTE3LzExLTA2LTIwMjYvUGVudG9yMjAyNi0xMS1KdW4tMjAyNi5tcDQ=/index-v1-a1.m3u8'
+  },
+   {
+    name: 'เป็นต่อ 2026 ตอนที่ 23',
+    group: 'comedy',
+    logo: 'https://image.onehd.net/Media/Show/263012013053241.jpg',
+    url: 'https://cdn.watchlakorn.in/egde/b53467be0eb3414b91f31b7d7ba419ff/NzAwNw==/MjM=/ZGF0YTE3LzE4LTA2LTIwMjYvUGVudG9yMjAyNi0xOC1KdW4tMjAyNi5tcDQ=/index-v1-a1.m3u8'
+  },
+   
+   {
+    name: 'เป็นต่อ 2026 ตอนที่ 25',
+    group: 'comedy',
+    logo: 'https://image.onehd.net/Media/Show/263012013053241.jpg',
+    url: 'https://cdn.watchlakorn.in/egde/0a3fea086f238b0e318ffebabda6b653/NzAwNw==/MjU=/ZGF0YTE3LzAyLTA3LTIwMjYvUGVudG9yMjAyNi0yLUp1bC0yMDI2Lm1wNA==/index-v1-a1.m3u8'
+  },
+   {
+    name: 'เป็นต่อ 2026 ตอนที่ 26',
+    group: 'comedy',
+    logo: 'https://image.onehd.net/Media/Show/263012013053241.jpg',
+    url: 'https://cdn.watchlakorn.in/egde/b533475791090ca9b483a8625eeb250a/NzAwNw==/MjY=/ZGF0YTE3LzA5LTA3LTIwMjYvUGVudG9yMjAyNi05LUp1bC0yMDI2Lm1wNA==/index-v1-a1.m3u8'
+  },
+];
